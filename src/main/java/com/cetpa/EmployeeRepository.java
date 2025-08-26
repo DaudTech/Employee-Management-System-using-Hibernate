0d099b06a66156f81865b0eb42af856a13722e15
Mo Daud Raza <161719681+DaudTech@users.noreply.github.com>
2025-08-26 19:20:13 +0530

    Update EmployeeRepository.java

--- src/main/java/com/cetpa/EmployeeRepository.java
+++ src/main/java/com/cetpa/EmployeeRepository.java
@@ -1,61 +1,74 @@
 package com.cetpa;
 
 import java.util.List;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 
-public class EmployeeRepository 
-{
-	static Session ses;
-	static Transaction trn;
-	static 
-	{
-		SessionFactory fac=new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
-		ses=fac.openSession();
-		trn=ses.getTransaction();
-	}
-	public static void saveEmployee(Employee emp)
-	{
-		trn.begin();
-		ses.save(emp);
-		trn.commit();
-	}
-	public static void deleteEmployee(Employee emp)
-	{
-		
-		trn.begin();
-		ses.delete(emp);
-		trn.commit();
-	}
-	public static void updateEmployee(Employee empnew, Employee empold)
-	{
-		
-		trn.begin();
-		empold.setFirstname(empnew.getFirstname());
-		empold.setLastname(empnew.getLastname());
-		empold.setPhone(empnew.getPhone());
-		empold.setEmailid(empnew.getEmailid());
-		empold.setDepartment(empnew.getDepartment());
-		empold.setSalary(empnew.getSalary());
-		trn.commit();
-	}
-	public static List<Employee> getEmployeeList()
-	{
-        Query<Employee> query=ses.createQuery("from Employee",Employee.class);
-        List<Employee> employeeList=query.list();
-        return employeeList;
-       
-	}
-	
-	public static Employee getEmployeeEid(int eid)
-	{
-		Employee employee=ses.get(Employee.class,eid);
-		return employee;
-	}
-	
+public class EmployeeRepository {
 
+    private static SessionFactory factory;
+
+    static {
+        try {
+            factory = new Configuration()
+                        .configure("hibernate.cfg.xml")
+                        .buildSessionFactory();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ExceptionInInitializerError("SessionFactory initialization failed");
+        }
+    }
+
+    private static Session getSession() {
+        return factory.openSession();
+    }
+
+    public static void saveEmployee(Employee emp) {
+        try (Session session = getSession()) {
+            Transaction tx = session.beginTransaction();
+            session.save(emp);
+            tx.commit();
+        }
+    }
+
+    public static void deleteEmployee(Employee emp) {
+        try (Session session = getSession()) {
+            Transaction tx = session.beginTransaction();
+            session.delete(emp);
+            tx.commit();
+        }
+    }
+
+    public static void updateEmployee(Employee empnew, Employee empold) {
+        try (Session session = getSession()) {
+            Transaction tx = session.beginTransaction();
+            Employee employee = session.get(Employee.class, empold.getEid());
+            if (employee != null) {
+                employee.setFirstname(empnew.getFirstname());
+                employee.setLastname(empnew.getLastname());
+                employee.setPhone(empnew.getPhone());
+                employee.setEmailid(empnew.getEmailid());
+                employee.setDepartment(empnew.getDepartment());
+                employee.setSalary(empnew.getSalary());
+                session.update(employee);
+            }
+            tx.commit();
+        }
+    }
+
+    public static List<Employee> getEmployeeList() {
+        try (Session session = getSession()) {
+            Query<Employee> query = session.createQuery("from Employee", Employee.class);
+            return query.list();
+        }
+    }
+
+    public static Employee getEmployeeEid(int eid) {
+        try (Session session = getSession()) {
+            return session.get(Employee.class, eid);
+        }
+    }
 }
